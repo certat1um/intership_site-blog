e6178d89c62bafdc6b48783e30405575e84a17c9
certat1um <canvasworkst@gmail.com>
2023-01-02 21:44:55 +0200

    Deleted unnecessary readFile.ts

--- src/helpers/readFile.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const createPath = require('./createPath');
-
-const readFile = (filename: string, callback: any): any => {
-  fs.readFile(createPath(filename), 'utf-8', callback);
-};
-
-module.exports = readFile;
